5900445d90e42fedda256f21b5e8f0e9378fdf3b
dsurdiy <dsurdiy@ukr.net>
2025-03-22 20:38:38 +0200

    tours section added

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.button-prev',
   },
 });
+
+const items = document.querySelectorAll(".tours__item");
+
+items.forEach(element => {
+
+  element.addEventListener("click", () => {
+    items.forEach(el => {
+      el.classList.remove("tours__item--active");
+    });
+
+    element.classList.add("tours__item--active");
+  });
+
+});
